1d886b3951dac17f041f3633896e2aaefd8653a2
rahuang <rahuang@andrew.cmu.edu>
2016-01-07 11:14:01 -0500

    hello world and get accounts

--- carrot/cloud/main.js
+++ carrot/cloud/main.js
@@ -4,3 +4,20 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!lslofkajhlsfkjahlsdkjfh");
 });
+
+
+Parse.Cloud.define("getAccounts", function(request, response) {
+    Parse.Cloud.httpRequest({
+      url: 'http://api.reimaginebanking.com/accounts',
+      params: {
+        key : 'd050a1874e89b27881665db1d0352daa'
+      }
+    }).then(function(httpResponse) {
+      // success
+      console.log(httpResponse.text);
+    },function(httpResponse) {
+      // error
+      console.error('Request failed with response code ' + httpResponse.status);
+    });
+}
+
