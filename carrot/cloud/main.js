c569b97e40bfdfedf8ddd84bddd339ffaf8b0637
Richard Ouyang <richardlouyang@gmail.com>
2016-01-07 23:10:39 -0500

    fuck yeah fixed that nessie post crap

--- carrot/cloud/main.js
+++ carrot/cloud/main.js
@@ -1,7 +1,7 @@
 //customer_id = 56241a13de4bf40b1711222c
 //account_id = 56241a14de4bf40b17112eae
 var nessieKey = '93da98350c71eb1daca8329c990c50e0';
-
+var carrotMasterId = '568f15e43921211200ef2143';
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
@@ -51,7 +51,7 @@ Parse.Cloud.define("getPurchasesForUser", function(request, response) {
         error: function(httpResponse) {
           // error
           console.error('Request failed with response code ' + httpResponse.status);
-          response.error(status);
+          response.error(httpRequest.status);
         }
       });
     },
@@ -95,7 +95,7 @@ Parse.Cloud.define("getTotalSpendingChange", function(request, response) {
         error: function(httpResponse) {
           // error
           console.error('Request failed with response code ' + httpResponse.status);
-          response.error(status);
+          response.error(httpResponse.status);
         }
       });
     },
@@ -132,7 +132,7 @@ Parse.Cloud.define("getMostRecentSpareChange", function(request, response) {
         error: function(httpResponse) {
           // error
           console.error('Request failed with response code ' + httpResponse.status);
-          response.error(status);
+          response.error(httpResponse.status);
         }
       });
 
@@ -188,7 +188,7 @@ Parse.Cloud.define("processPurchases", function(request, response) {
         error: function(httpResponse) {
           // error
           console.error('Request failed with response code ' + httpResponse.status);
-          response.error(status);
+          response.error(httpResponse.status);
         }
       });
     },
@@ -198,11 +198,13 @@ Parse.Cloud.define("processPurchases", function(request, response) {
   });
 
 });
-
-
+/*
 Parse.Cloud.define("addToCarrot", function(request, response) {
   var query = new Parse.Query("User");
+  var amount = request.params.amount;
+  var change = roundToTwo(Math.ceil(amount) - amount);
   query.equalTo("objectId", request.params.object_id);
+
   query.find({
     success: function(results) {
       var carrot_id = results[0].get("carrot_id");
@@ -217,21 +219,144 @@ Parse.Cloud.define("addToCarrot", function(request, response) {
         },
         body: {
           "medium": "balance",
-          "transaction_date": "2016-01-08",
+          "transaction_date": "2016-01-09",
           "status": "pending",
-          "amount": 80, // needs to be in cents
-          "description": "free money"
+          "amount": 100 * change, // needs to be in cents because of Nessie API. Keep watch for a fix
+          "description": "Carrot Savings Deposit"
         },
         success: function(httpResponse) {
           console.log("success! deposited change into carrot!");
-          response.success("hooray!");
+          response.success("hooray! deposited into carrot!");
         },
         error: function(httpResponse) {
           // error
           console.error('Request failed with response code ' + httpResponse.status);
-          response.error(status);
+          response.error(httpResponse.status);
         }
       });
+    },
+    error: function() {
+      response.error("Account lookup failed");
+    }
+  });
+
+});
+*/
+
+// pass in amount and object_id
+Parse.Cloud.define("processNewPurchase", function(request, response) {
+  var query = new Parse.Query("User");    
+  var amount = request.params.amount;
+  var change = roundToTwo(Math.ceil(amount) - amount);
+  var merchant_id = "56241a13de4bf40b17112337";
+  var description = "ice cream";
+
+  query.equalTo("objectId", request.params.object_id);
+  query.find({
+    success: function(results) {
+      var account_id = results[0].get("account_id");
+      var carrot_id = results[0].get("carrot_id");
+
+      var apiUrl = 'http://api.reimaginebanking.com/accounts/' + account_id + "/purchases?key=" + nessieKey;
+
+      // charge (normal) amount to card
+      Parse.Cloud.httpRequest({
+        method: "POST",
+        url: apiUrl,
+        // params: {
+        //   key : nessieKey
+        // },
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: {
+          "merchant_id": merchant_id,
+          "medium": "balance",
+          "purchase_date": "2016-01-09",
+          "amount": amount, 
+          "status": "pending",
+          "description": description 
+        },
+        success: function(httpResponse) {
+          console.log(httpResponse.text);
+          console.log("success! charge amount to account!");
+          // charge change to card
+          Parse.Cloud.httpRequest({
+            method: "POST",
+            url: apiUrl,
+            // params: {
+            //   key : nessieKey
+            // },
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: {
+              "merchant_id": carrotMasterId,
+              "medium": "balance",
+              "purchase_date": "2016-01-09",
+              "amount": change, 
+              "status": "pending",
+              "description": "Carrot Savings Charge"
+            },
+            success: function(httpResponse) {
+              console.log(httpResponse.text);
+              console.log("success! charge change to account!");
+              
+              // add to carrot savings
+
+              var carrotDepositUrl = 'http://api.reimaginebanking.com/accounts/' + carrot_id + "/deposits?key=" + nessieKey;
+
+              Parse.Cloud.httpRequest({
+                method: "POST",
+                url: carrotDepositUrl,
+                // params: {
+                //   key : nessieKey
+                // },
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: {
+                  "medium": "balance",
+                  "transaction_date": "2016-01-09",
+                  "status": "pending",
+                  "amount": 100 * change, // needs to be in cents because of Nessie API. Keep watch for a fix
+                  "description": "Carrot Savings Deposit"
+                },
+                success: function(httpResponse) {
+                  console.log(httpResponse.text);
+                  console.log("success! deposited change into carrot!");
+                  response.success("hooray! new purchase processed!");
+                },
+                error: function(httpResponse) {
+                  // error
+                  console.log(httpResponse.text);
+
+                  console.error('inner Request failed with response code ' + httpResponse.status);
+                  response.error(httpResponse.status);
+                }
+              });
+
+            },
+            error: function(httpResponse) {
+              // error
+              console.log(httpResponse.text);
+              console.error('middle Request failed with response code ' + httpResponse.status);
+              response.error(httpResponse.status);
+            }
+          });
+
+
+
+        },
+        error: function(httpResponse) {
+          // error
+          console.log(httpResponse.text);
+
+          console.error('outer Request failed with response code ' + httpResponse.status + httpResponse.text);
+          response.error(httpResponse.status);
+        }
+      });
+
     },
     error: function() {
       response.error("Account lookup failed");
@@ -262,7 +387,7 @@ Parse.Cloud.define("getCustomerFromObjectId", function(request, response) {
         error: function(httpResponse) {
           // error
           console.error('Request failed with response code ' + httpResponse.status);
-          response.error(status);
+          response.error(httpRequest.status);
         }
       });
     },
--- carrot/cloud/richard.js
+++ carrot/cloud/richard.js
@@ -2,3 +2,4 @@
 // richard's account id 56241a14de4bf40b17112f02
 // richard's merchant id 56241a13de4bf40b17112337
 // richard's carrot id 568ee77c3921211200ef2115
+// richard's carrot master merchant id 568f15e43921211200ef2143
